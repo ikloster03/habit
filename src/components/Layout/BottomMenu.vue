223a4261ec57f2074a7c63a5b32685a1ce3f4aa0
Ivan Monastyrev <ikloster@yandex.ru>
2021-02-03 22:55:22 +0300

    add bottom menu

---
+++ src/components/Layout/BottomMenu.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-bottom-navigation v-model="value" :background-color="color" dark shift>
+    <v-btn>
+      <span>Home</span>
+      <v-icon>mdi-home</v-icon>
+    </v-btn>
+    <v-btn>
+      <span>Settings</span>
+      <v-icon>mdi-cog</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+  name: 'BottomMenu',
+  data: () => ({ value: 1 }),
+
+  computed: {
+    color() {
+      switch (this.value) {
+        case 0:
+          return 'brown';
+        case 1:
+          return 'blue-grey';
+        default:
+          return 'blue-grey';
+      }
+    },
+  },
+};
+</script>
+
+<style scoped></style>
