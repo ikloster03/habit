9ee18e2054f4e3a46b0fa58bf82c8b91d7a83db9
Ivan Monastyrev <ikloster@yandex.ru>
2021-02-04 20:00:55 +0300

    screen(create-habit): add header + habit-form

--- src/screens/CreateHabitScreen.vue
+++ src/screens/CreateHabitScreen.vue
@@ -1,10 +1,25 @@
 <template>
-  <div></div>
+  <v-container>
+    <div class="d-flex align-center pb-4">
+      <v-btn fab color="blue" dark :to="{ name: 'home-screen' }">
+        <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
+      <h2 class="pl-4">Creating habit</h2>
+    </div>
+    <habit-form @form-data="saveHabit" />
+  </v-container>
 </template>
 
 <script>
+import HabitForm from '@/components/Habit/HabitForm.vue';
 export default {
   name: 'CreateHabitScreen',
+  components: { HabitForm },
+  methods: {
+    saveHabit(habit) {
+      console.log('habit', habit);
+    },
+  },
 };
 </script>
 
