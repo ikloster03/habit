5e7537bc52ac1bf5de20dfe90e2e89ade3915d23
Ivan Monastyrev <ikloster@yandex.ru>
2021-02-07 23:16:21 +0300

    screen(settings): add import/export habits

--- src/screens/SettingsScreen.vue
+++ src/screens/SettingsScreen.vue
@@ -1,10 +1,41 @@
 <template>
-  <div></div>
+  <div>
+    <v-btn color="blue" @click="exportHabits">Export habits</v-btn>
+    <br />
+    <input type="file" @change="handleFiles" />
+    <v-btn color="orange" @click="importHabits">Import habits</v-btn>
+  </div>
 </template>
 
 <script>
+import { downloadFile, readFile } from '@/helpers/file';
+
 export default {
   name: 'SettingsScreen',
+  data() {
+    return {
+      file: null,
+    };
+  },
+  methods: {
+    exportHabits() {
+      const stringifiedHabits = localStorage.getItem('habits');
+      if (stringifiedHabits) {
+        const fileName = 'habits-backup';
+        const fileType = 'json';
+        downloadFile({ data: stringifiedHabits, fileName, fileType });
+      }
+    },
+    handleFiles(event) {
+      this.file = event.target.files[0];
+    },
+    async importHabits() {
+      if (this.file) {
+        const habits = await readFile(this.file);
+        localStorage.setItem('habits', String(habits));
+      }
+    },
+  },
 };
 </script>
 
