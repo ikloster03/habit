87569379e32de919e57862fe68b536d7ef1da7a8
Ivan Monastyrev <ikloster@yandex.ru>
2021-02-08 23:16:40 +0300

    helpers: add habit

---
+++ src/helpers/habit.js
@@ -0,0 +1,26 @@
+import { parseISO, compareAsc } from 'date-fns';
+
+const calculateScore = index => {
+  if (index >= 0 && index <= 30) {
+    return index * 2;
+  } else if (index > 30 && index <= 60) {
+    return index - 30 + 60;
+  } else if (index > 60 && index <= 100) {
+    return (1 / 3) * (index - 60) + 90;
+  }
+
+  return -1;
+};
+
+const getHabitDateScoreMap = dates => {
+  const parsedDates = dates.map(date => parseISO(date));
+  const sortedDates = parsedDates.sort(compareAsc);
+
+  return sortedDates.map((date, index) => ({
+    // date: format(date, 'MM dd'),
+    date: date,
+    score: calculateScore(index),
+  }));
+};
+
+export default getHabitDateScoreMap;
