e2b25bfee232db8fd9357aa53e8eeea16e77f41b
Ivan Monastyrev <ikloster@yandex.ru>
2021-02-10 23:58:17 +0300

    main: fix code style

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import vuetify from "./plugins/vuetify";
-import "roboto-fontface/css/roboto/roboto-fontface.css";
-import "@mdi/font/css/materialdesignicons.css";
-import 'vue2-dropzone/dist/vue2Dropzone.min.css'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import '@mdi/font/css/materialdesignicons.css';
+import 'vue2-dropzone/dist/vue2Dropzone.min.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   vuetify,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app');
