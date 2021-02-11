1b2c475ab5419a0a322d9b544979c50c191f53ff
Ivan Monastyrev <ikloster@yandex.ru>
2021-02-11 23:43:09 +0300

    main: add i18n

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import vuetify from './plugins/vuetify';
+import i18n from './i18n';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
 import 'vue2-dropzone/dist/vue2Dropzone.min.css';
@@ -12,5 +13,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   vuetify,
+  i18n,
   render: h => h(App),
 }).$mount('#app');
