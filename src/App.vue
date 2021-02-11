a15830283cb2d7b0e1f8b631a999a81284dd6f65
Ivan Monastyrev <ikloster@yandex.ru>
2021-02-11 23:44:31 +0300

    app: add locale loader from local storage

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,18 @@
 import BottomMenu from '@/components/Layout/BottomMenu.vue';
 export default {
   name: 'App',
-
   components: { BottomMenu },
-
   data: () => ({
     //
   }),
+  created() {
+    const locale = localStorage.getItem('locale');
+
+    if (locale) {
+      this.$i18n.locale = locale;
+    } else {
+      localStorage.setItem('locale', this.$i18n.locale);
+    }
+  },
 };
 </script>
