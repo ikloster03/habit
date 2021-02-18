369eae7e5d7174ba0c9e4c5d1803c974d89405bc
Ivan Monastyrev <ikloster@yandex.ru>
2021-02-18 23:01:53 +0300

    modules(habit): add calculateScore, getDateScoreMap

--- src/modules/habit/habit.js
+++ src/modules/habit/habit.js
@@ -1,10 +1,23 @@
 import { v4 as uuidv4 } from 'uuid';
+import { parseISO, compareAsc } from 'date-fns';
 
 const defaultHabit = {
   title: '',
   description: '',
   dates: [],
   color: '#000000',
+};
+
+const calculateScore = index => {
+  if (index >= 0 && index <= 30) {
+    return index * 2;
+  } else if (index > 30 && index <= 60) {
+    return index - 30 + 60;
+  } else if (index > 60 && index <= 100) {
+    return (1 / 3) * (index - 60) + 90;
+  }
+
+  return -1;
 };
 
 class Habit {
@@ -29,6 +42,24 @@ class Habit {
       description: this.description,
       dates: this.dates,
       color: this.color,
+    };
+  }
+
+  getDateScoreMap() {
+    const parsedDates = this.dates.map(date => parseISO(date));
+    const sortedDates = parsedDates.sort(compareAsc);
+
+    const list = sortedDates.map((date, index) => ({
+      date: date,
+      score: calculateScore(index),
+    }));
+    const scores = list.map(s => s.score);
+    const percent = scores[scores.length - 1];
+
+    return {
+      list,
+      scores,
+      percent,
     };
   }
 }
