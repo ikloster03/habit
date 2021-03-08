7ffc84f3ccaeb95c0fa15267f512c4467ad8daf0
Ivan Monastyrev <ikloster@yandex.ru>
2021-03-08 23:07:07 +0300

    fix safari bottom menu issue

--- src/components/Layout/BottomMenu.vue
+++ src/components/Layout/BottomMenu.vue
@@ -1,20 +1,37 @@
 <template>
-  <v-bottom-navigation v-model="value" :background-color="color" fixed dark shift>
-    <v-btn exact :to="{ name: 'home-screen' }">
-      <span>{{ $t('menu.home') }}</span>
-      <v-icon>mdi-home</v-icon>
-    </v-btn>
-    <v-btn :to="{ name: 'settings-screen' }">
-      <span>{{ $t('menu.settings') }}</span>
-      <v-icon>mdi-cog</v-icon>
-    </v-btn>
+  <v-bottom-navigation
+    v-model="value"
+    :background-color="color"
+    :height="isMobSafari ? mobSafariHeight : baseHeight"
+    fixed
+    dark
+    shift
+  >
+    <div>
+      <div class="d-flex" :style="{ height: `${baseHeight}px` }">
+        <v-btn exact :to="{ name: 'home-screen' }">
+          <span>{{ $t('menu.home') }}</span>
+          <v-icon>mdi-home</v-icon>
+        </v-btn>
+        <v-btn :to="{ name: 'settings-screen' }">
+          <span>{{ $t('menu.settings') }}</span>
+          <v-icon>mdi-cog</v-icon>
+        </v-btn>
+      </div>
+      <div v-if="isMobSafari" :style="{ height: `${mobSafariHeightEmptyBlock}px` }"></div>
+    </div>
   </v-bottom-navigation>
 </template>
 
 <script>
 export default {
   name: 'BottomMenu',
-  data: () => ({ value: 0 }),
+  data: () => ({
+    value: 0,
+    windowWidth: 0,
+    baseHeight: 56,
+    mobSafariHeight: 70,
+  }),
 
   computed: {
     color() {
@@ -26,6 +43,29 @@ export default {
         default:
           return '#546E7AFF';
       }
+    },
+    mobSafariHeightEmptyBlock() {
+      return this.mobSafariHeight - this.baseHeight;
+    },
+    isSafari() {
+      return /iPhone|iPad|iPod|/i.test(navigator.userAgent);
+    },
+    isMobSafari() {
+      return this.isSafari && this.windowWidth < 900 && this.windowWidth > 0;
+    },
+  },
+  created() {
+    window.addEventListener('resize', this.windowResize);
+  },
+  mounted() {
+    this.windowResize();
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.windowResize);
+  },
+  methods: {
+    windowResize() {
+      this.windowWidth = document.documentElement.clientWidth;
     },
   },
 };
