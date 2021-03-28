3e58f864de85b001d735a1d383ed510cbbed6494
Ivan Monastyrev <ikloster@yandex.ru>
2021-03-28 23:01:35 +0300

    vue-config: update pwa

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,19 @@ module.exports = {
     },
   },
   pwa: {
+    name: 'HabitSync',
+    themeColor: '#546e7a',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    iconPaths: {
+      favicon32: 'habit-sync-logo.png',
+      favicon16: null,
+      appleTouchIcon: null,
+      maskIcon: null,
+      msTileImage: null,
+    },
+    workboxPluginMode: 'InjectManifest',
     workboxOptions: {
       skipWaiting: true,
     },
